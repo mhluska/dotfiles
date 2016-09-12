781e6985f38f2b1cf2d2202099ea077844d23062
Maros Hluska <mhluska@gmail.com>
2016-09-12 15:29:39 -0700

    Add pyenv to babushka

--- .babushka/deps/dotfiles.rb
+++ .babushka/deps/dotfiles.rb
@@ -1,3 +1,6 @@
+dep 'how2.npm'
+dep 'pyenv.lib'
+
 dep 'dotfiles' do
-  requires 'homebrew', 'tmux.src', 'how2.npm'
+  requires 'homebrew', 'tmux.src', 'how2.npm', 'pyenv.lib'
 end
--- .babushka/deps/how2.rb
+++
@@ -1,1 +0,0 @@
-dep 'how2.npm'
